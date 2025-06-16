9fbfef69ba1261e7290d15d4bc400d3ddea84628
phuocbt <phuocbt9x@gmail.com>
2025-06-17 01:17:51 +0700

    build base project

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class BaseModel extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
+
+    protected $primaryKey = "uuid";
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+}
